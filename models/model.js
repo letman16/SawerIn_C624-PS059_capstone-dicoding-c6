4bae243c7cbb6a6943fe65c621f9a54c22fbab4b
Lukman Hakim <letmantech@gmail.com>
2024-06-18 05:55:22 +0700

    model mysql 4

--- models/model.js
+++ models/model.js
@@ -1,5 +1,14 @@
 import { Sequelize, DataTypes} from 'sequelize';
 
+// Konfigurasi MySQL2 tanpa opsi timeout
+const conn = mysql2.createConnection({
+    host: process.env.HOST,
+    user: process.env.USERDB,
+    password: process.env.PASSDB,
+    database: process.env.DBNAME
+});
+
+// Konfigurasi Sequelize dengan dialectOptions
 const sequelize = new Sequelize(process.env.DBNAME, process.env.USERDB, process.env.PASSDB, {
     host: process.env.HOST,
     dialect: 'mysql',
@@ -7,15 +16,5 @@ const sequelize = new Sequelize(process.env.DBNAME, process.env.USERDB, process.
         connectTimeout: 60000 // Contoh timeout 60 detik
     }
 });
-
-// Test connection
-async function testConnection() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection to database has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
 
 export { sequelize, testConnection, DataTypes };
